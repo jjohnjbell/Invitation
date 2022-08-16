e813ba4cf57177b3774a87a1a34bcba8ee753fc9
Joshua Bell <83473305+jjohnjbell@users.noreply.github.com>
2022-08-16 13:10:13 -0500

    commit on invitation2.0

--- JS/index.js
+++ JS/index.js
@@ -6,27 +6,27 @@ const mainContainer = document.getElementById("mainContainer")
 const card = document.getElementById("card")
 
 //Add Event to Details Button
-// showInviteBtn.addEventListener("click", function (){
-//     console.log("hello")
-//     // swap()
-// })
+showInviteBtn.addEventListener("click", function (){
+    showDetails.style.transform="rotateY(360deg)"
+    card.style.transform="rotateY(360deg)"
+    console.log("hello")
+   // swap()
+})
 
 // //Add Event to Invite Button
 showDetailsBtn.addEventListener("click", function (){
     console.log("details")
+    // swap()
     showInvite.style.transform="rotateY(180deg)"
     card.style.transform="rotateY(180deg)"
-   // mainContainer.style.transform="rotateY(180deg)"
-    
-    // swap()
 })
 
-// function swap(){
-//     showDetails.style.transform="rotateY(180deg)"
-//     showInvite.style.transform="rotateY(180deg)"
-//     card.style.transform="rotateY(180deg)"
-//     mainContainer.style.transform="rotateY(180deg)"
-// }
+function swap(){
+    showDetails.style.transform="rotateY(180deg)"
+    showInvite.style.transform="rotateY(180deg)"
+    card.style.transform="rotateY(180deg)"
+    // mainContainer.style.transform="rotateY(180deg)"
+}
 
 /*.mainContainer:hover .card {
     transform: rotateY(180deg);
