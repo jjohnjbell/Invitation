e592e015822cc9016044e5fd483867efd4b2702a
Joshua Bell <83473305+jjohnjbell@users.noreply.github.com>
2022-08-28 16:37:37 -0500

    commit to invitation2.0

--- JS/index.js
+++ JS/index.js
@@ -2,11 +2,17 @@ const hashTag = document.getElementById("hashTag")
 const firstCont= document.getElementById("firstSection")
 const bellsEl = document.getElementById("bells")
 const popUp = document.getElementById("popUp")
+const closeEl = document.getElementById("close")
 
 bellsEl.addEventListener("click", function(){
     hashTag.style.animation="ring 1s linear 1"
     hashTag.style.animationFillMode="forwards"
-    firstCont.style.animation="hearts 10s linear 2"
+    popUp.style.animation="hearts 10s linear 2"
     popUp.style.visibility="visible"
 
+})
+
+closeEl.addEventListener("click",function(){
+    popUp.style.visibility="hidden"
+    console.log("button works")
 })
