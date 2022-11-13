b4dc818ba616e51bc73b3be05feed4eb383111a3
Joshua Bell <83473305+jjohnjbell@users.noreply.github.com>
2022-11-13 13:33:03 -0500

    commit on main

--- JS/index.js
+++ JS/index.js
@@ -8,10 +8,12 @@ bellsEl.addEventListener("click", function(){
     hashTag.style.animation="ring 1s linear 1"
     hashTag.style.animationFillMode="forwards"
     weddingLocation.style.animation="hearts 10s linear 2"
-    weddingLocation.style.visibility="visible"
+    // weddingLocation.style.visibility="visible"
+    weddingLocation.style.display="block"
 
 })
 
 closeEl.addEventListener("click",function(){
-    weddingLocation.style.visibility="hidden"
+    // weddingLocation.style.visibility="hidden"
+    weddingLocation.style.display="none"
 })
