21df26e2996f08ee67c8c160a671e68a4e9807f1
Joshua Bell <83473305+jjohnjbell@users.noreply.github.com>
2022-11-13 19:17:50 -0500

    commit on main

--- JS/index.js
+++ JS/index.js
@@ -4,15 +4,15 @@ const bellsEl = document.getElementById("bells")
 const weddingLocation = document.getElementById("invitedPersons")
 const closeEl = document.getElementById("close")
 
-bellsEl.addEventListener("dblclick", function(){
-    console.log("Pressed")
-    // hashTag.style.animation="ring 1s linear 1"
-    // hashTag.style.animationFillMode="none"
-    weddingLocation.style.animation="hearts 10s linear 2"
-    // weddingLocation.style.visibility="visible"
-    weddingLocation.style.display="block"
+// bellsEl.addEventListener("dblclick", function(){
+//     console.log("Pressed")
+//     // hashTag.style.animation="ring 1s linear 1"
+//     // hashTag.style.animationFillMode="none"
+//     weddingLocation.style.animation="hearts 10s linear 2"
+//     // weddingLocation.style.visibility="visible"
+//     weddingLocation.style.display="block"
 
-})
+// })
 
 closeEl.addEventListener("click",function(){
     // weddingLocation.style.visibility="hidden"
